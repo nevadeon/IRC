b8a53847d888a4af4c9a3edc4bb918491b86d9fb
Manu <you@example.com>
2025-10-01 15:25:59 +0200

    bug valgrind

--- include/Message.hpp
+++ include/Message.hpp
@@ -2,17 +2,20 @@
 #define MESSAGE_HPP
 
 #include <string>
+#include <vector>
 
 class Message
 {
     private:
-        int sender;
-        int receiver;
-        std::string content;
+        int sender_; //socket fd
+        std::string receiver_; //nickname
+        std::vector<std::string> content;
         std::string command;
 
     public:
-        Message(int fd, const char *buffer);
+        Message(int fd);
 };
+
+Message::Message(int fd) : sender_(fd) {}
 
 #endif

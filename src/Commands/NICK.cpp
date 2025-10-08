175a8dbbccb59140228ef4042607e91416225e85
Manu <you@example.com>
2025-10-08 21:02:41 +0200

    je continue les commandes en attendant le parsing

--- src/Commands/NICK.cpp
+++ src/Commands/NICK.cpp
@@ -1,4 +1,25 @@
 #include "Server.hpp"
+#include <set>
+
+static const char disallowedCharsArray[] = { ' ', ',', '*', '?', '!', '@' };
+static const int disallowedCharsCount = sizeof(disallowedCharsArray) / sizeof(disallowedCharsArray[0]);
+std::set<char> disallowedChars(disallowedCharsArray, disallowedCharsArray + disallowedCharsCount);
+
+static const char disallowedStartCharsArray[] = { '$', ':', '#', '&', '+', '%', '~', '@', '!' };
+static const int disallowedStartCharsCount = sizeof(disallowedStartCharsArray) / sizeof(disallowedStartCharsArray[0]);
+std::set<char> disallowedStartChars(disallowedStartCharsArray, disallowedStartCharsArray + disallowedStartCharsCount);
+
+bool isValidNickname(const std::string& nickname) {
+    if (nickname.empty()) return false;
+
+    for (std::string::size_type i = 0; i < nickname.length(); ++i) {
+        if (disallowedChars.find(nickname[i]) != disallowedChars.end()) return false;
+    }
+
+    if (disallowedStartChars.find(nickname[0]) != disallowedStartChars.end()) return false;
+
+    return true;
+}
 
 /*
     ARGS: [COMMAND = NICK] <nick>
@@ -8,28 +29,25 @@
 */
 int Server::Commands::NICK(Server& server, int fd, std::vector<std::string>& args)
 {
-    if (!server.clients_.count(fd)) return (1);
+    if (!server.clients_.count(fd))
+        return (1); // Exception: no fd in clients
     Client c = server.clients_[fd];
     if (args.size() < 2)
     {
-
-        return (0); //431
+        //431
+        return (0); 
     }
     std::string nickname = args[1];
     if (server.IsNicknameAlreadyUsed(nickname)) // Check if valid nickname (case-insensitive)
     {
+        //433
+        return (0);
+    }
+    if (!isValidNickname(nickname)) //Also check length, if begins with letter, no special characters, no whitespace
+    {
         //432
-        return (1);
+        return (0);
     }
-    //Also check length, if begins with letter, no special characters, no whitespace
-
-    if (server.clients_.count(fd))
-    {
-
-    }
-    else // Person is registered, check and set
-    {
-
-    }
-    return (0);
+    
+   return (0);
 }
--- src/Commands/USER.cpp
+++ src/Commands/USER.cpp
@@ -1,9 +1,37 @@
 #include "Server.hpp"
 
+
+/*
+    Parameters: <username> 0 * <realname> -> 5 args including command
+    The minimum length of <username> is 1, ie. it MUST NOT be empty.
+    !! REALNAME MAY CONTAIN SPACES
+*/
 int Server::Commands::USER(Server& server, int fd, std::vector<std::string>& args)
 {
-    (void)server;
-    (void)fd;
-    (void)args;
-    return 0;
+    if (args.size() < 5)
+    {
+        // ERR_NEEDMOREPARAMS
+        return (0);
+    }
+
+    if (server.clients_[fd].GetUserInfoGiven())
+    {
+        // ERR_ALREADYREGISTERED
+        return (0);
+    }
+
+    struct user_info info;
+    info.username = args[1];
+    if (info.username.size() == 0)
+    {
+        // ERR_NEEDMOREPARAMS
+        return (1);
+    }
+    
+    info.hostname = args[2];
+    info.servername = args[3];
+    info.realname = args[4];
+    server.clients_[fd].SetUserInfo(info);
+
+    return (0);
 }

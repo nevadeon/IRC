6078fbee3e5fc183db468e2646b2a168756ec3ee
Manu <you@example.com>
2025-10-12 21:13:40 +0200

    MODE progress

--- src/Commands/MODE.cpp
+++ src/Commands/MODE.cpp
@@ -1,9 +1,49 @@
 #include "Server.hpp"
 
+static bool IsAChanOp(Channel* ch, Client *cl)
+{
+    return (ch[cl] == IS_OPERATOR);
+}
+
+static bool IsKnownModeString(std::string& str)
+{
+
+}
+
+/*
+    ONLY ACCEPT ONE MODESTIRNG
+    Args: MODE <target> []
+*/
 int Server::Commands::MODE(Server& server, int fd, std::vector<std::string>& args)
 {
-    (void)server;
-    (void)fd;
-    (void)args;
-    return 0;
+    if (args.size() < 2)
+    {
+        // ERR_NEEDMOREPARAMS
+        return (0);
+    }
+
+    Channel *ch = server.FindChanel(args[1]);
+    if (!ch)
+    {
+        // 403: No such channel
+        return (0);
+    }
+
+    if (args.size() < 3)
+    {
+        // 324 RPL_CHANNELMODEIS
+        return (0);
+    }
+
+    if (!IsAChanOp(ch, &server.clients_[fd]))
+    {
+        // ERR_CHANOPRIVSNEEDED (482)
+    }
+
+    std::string modes = args[2];
+    if (IsKnownModeString(modes))
+    {
+
+    }
+
 }

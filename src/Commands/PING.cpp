ddaef2f56a6228a4540cf30c15a8ec2b8c17d24d
nevadeon <github@glhf.slmail.me>
2025-10-15 18:45:17 +0200

    asd

--- src/Commands/PING.cpp
+++ src/Commands/PING.cpp
@@ -2,7 +2,7 @@
 
 int Server::Commands::PING(Server& server, int fd, std::vector<std::string>& args)
 {
-    std::vector<std::string> params;
+    std::vector<std::sasdsasdtring> pasdsdrams;
     std::string returnValue = "PONG";
     if (args.size() > 1)
         params.push_back(args[1]);
